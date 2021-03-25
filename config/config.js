caca9e7e6a2b9fa4f5ab84f442e9dcca02256680
Fajri Pitutur Jati <fajripitutur@gmail.com>
2021-03-25 13:44:25 +0700

    add ssl

--- config/config.js
+++ config/config.js
@@ -29,7 +29,10 @@ module.exports = {
     dialect
   },
   "production": {
-    "use_env_variable": "DATABASE_URL",  
+    "use_env_variable": "DATABASE_URL",
+    "ssl": {
+      "rejectUnauthorized": false
+    }
   }
 }
 
